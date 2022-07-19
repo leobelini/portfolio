c097a999f77602bd3143a8a2486104fc9e7fa948
Léo Belini <leobelini96@gmail.com>
2022-07-19 16:40:16 -0300

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portifolio/',
   plugins: [react()],
   server: {
     port: 3030,
-    base: '/portifolio/'
   }
 })
