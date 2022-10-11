c8eeba2623ffec2275b49a05c5f95dec0b375ae6
Léo Belini <leobelini96@gmail.com>
2022-10-11 09:45:03 -0300

    Update Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -6,7 +6,36 @@ import {
   AiFillLinkedin,
   AiFillGithub,
 } from 'react-icons/ai'
+import { FaNodeJs } from 'react-icons/fa'
+import { RiHtml5Line, RiCss3Line, RiReactjsLine } from 'react-icons/ri'
+import {
+  SiAdonisjs,
+  SiElectron,
+  SiExpress,
+  SiMicrosoftsqlserver,
+  SiMongodb,
+  SiNestjs,
+  SiPostgresql,
+} from 'react-icons/si'
+import { TbBrandJavascript, TbBrandReactNative, TbCSharp } from 'react-icons/tb'
 import profile from '../../assets/profile.jpg'
+
+const listTechnology = [
+  { Icon: RiHtml5Line, name: `HTML5` },
+  { Icon: RiCss3Line, name: `CSS` },
+  { Icon: TbBrandJavascript, name: `JavaScript` },
+  { Icon: RiReactjsLine, name: `React` },
+  { Icon: TbBrandReactNative, name: `ReactNative` },
+  { Icon: FaNodeJs, name: `NodeJs` },
+  { Icon: SiAdonisjs, name: `AdonisJs` },
+  { Icon: SiNestjs, name: `NestJs` },
+  { Icon: SiExpress, name: `Express` },
+  { Icon: SiElectron, name: `Electron` },
+  { Icon: TbCSharp, name: `C#` },
+  { Icon: SiMongodb, name: `MongoDB` },
+  { Icon: SiMicrosoftsqlserver, name: `SQL Server` },
+  { Icon: SiPostgresql, name: `PostgreSQL` },
+]
 
 export const Home = () => {
   const greeting = useMemo(() => {
@@ -23,8 +52,8 @@ export const Home = () => {
   }, [])
 
   return (
-    <div className="text-gray-400 flex flex-row">
-      <div className="basis-3/5">
+    <div className="text-gray-400 grid grid-cols-2 gap-4">
+      <div className="xl:col-span-1 xl:order-1 col-span-2 order-2">
         <p className="text-lg my-2">{greeting}, tudo bem?</p>
         <p className="my-2">
           Meu nome é <b>Leonardo Belini</b>, sou Desenvolvedor Full Stack,
@@ -33,26 +62,32 @@ export const Home = () => {
           <Link href="https://sodretox.com.br" text="Sodré" />,{' '}
           <Link href="https://roberty.app" text="Roberty" /> e atualmente estou
           na <Link href="https://www.globalsys.com.br" text="Globalsys" />
+          {` `}
           como outsourcing.
         </p>
         <p className="my-2">
           Essa diversidade garantiu ampla experiência em diversas tecnologias
           como:
         </p>
-        <ul className="my-2 ml-8 list-disc">
+        <div className="flex flex-wrap">
+          {listTechnology.map((tec) => (
+            <IconTechnology key={tec.name} name={tec.name} Icon={tec.Icon} />
+          ))}
+        </div>
+        {/* <ul className="my-2 ml-8 list-disc">
           <li>Node/JavaScript/TypeScript;</li>
           <li>C#/.NET;</li>
           <li>NoSQL/MySQL/SQL Server;</li>
-        </ul>
+        </ul> */}
       </div>
-      <div className="basis-2/5">
+      <div className="xl:col-span-1 xl:mt-0 xl:order-2 col-span-2 mt-10 order-1">
         <div className="flex flex-1 justify-center items-center mb-10 flex-col">
           <img
             src={profile}
             alt="Foto de perfil de Leonardo Belini"
             className="rounded-full w-40"
           />
-          <div className="text-center mt-6">
+          <div className="text-center mt-6 xl:block hidden">
             <p className="py-2">
               <b>Nome: </b>Leonardo Siervo Belini
             </p>
@@ -104,7 +139,7 @@ const SocialLink: React.FC<SocialProps> = ({ Icon, ...props }) => {
   return (
     <a
       {...props}
-      className="mx-2 cursor-pointer	hover:text-carmine transition duration-700 hover:scale-150"
+      className="mx-2 cursor-pointer hover:text-carmine transition duration-300 hover:scale-150"
     >
       <Icon />
     </a>
@@ -125,6 +160,22 @@ const Link: React.FC<LinkProps> = ({ href, text }) => {
       rel="noopener noreferrer"
     >
       {text}
+    </a>
+  )
+}
+
+interface IconTechnologyProps {
+  Icon: IconType
+  href?: string
+  name: string
+  color?: string
+}
+
+const IconTechnology: React.FC<IconTechnologyProps> = ({ Icon, name }) => {
+  return (
+    <a>
+      <Icon className="h-7 w-7 m-2 hover:text-carmine transition duration-300 hover:scale-150" />
+      {/* {name} */}
     </a>
   )
 }
