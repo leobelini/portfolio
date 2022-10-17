dd28e2d0c8991ca64680ab20fb233c5f0bdb8358
Léo Belini <leobelini96@gmail.com>
2022-10-17 08:54:17 -0300

    feat: add button github

--- src/components/layout/IdeContext.tsx
+++ src/components/layout/IdeContext.tsx
@@ -1,6 +1,8 @@
 import { createContext, useMemo } from 'react'
 import { UseBoolean, useBoolean } from 'react-hanger'
 import { useLocation } from 'react-router-dom'
+import { AiFillGithub } from 'react-icons/ai'
+
 import { listRoutes } from '../../pages/routes'
 
 interface IdeContextType {
@@ -45,6 +47,15 @@ export const IdeProvider: React.FC<IdeProviderProps> = ({ children }) => {
       }}
     >
       {children}
+      {inCode.value && (
+        <a
+          href="https://github.com/leobelini/portfolio"
+          target={`_blank`}
+          className={`bg-carmine absolute right-10	 bottom-10 text-slate-100	w-10 h-10 rounded-full text-3xl flex items-center justify-center transition duration-300 hover:scale-110`}
+        >
+          <AiFillGithub />
+        </a>
+      )}
     </IdeContext.Provider>
   )
 }
