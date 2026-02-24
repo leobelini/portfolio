f51e76341fbbd98e1bfeeb4bfb52514eb40d2090
Léo Belini <leo.belini@globalsys.com.br>
2026-02-24 19:25:30 -0300

    clean

--- src/components/isTrue/IsTrue.tsx
+++
@@ -1,8 +0,0 @@
-type IsTrueProps = {
-  isTrue: boolean
-  children: React.ReactNode
-}
-
-export const IsTrue: React.FC<IsTrueProps> = ({ children, isTrue }) => {
-  return isTrue ? <>{children}</> : null
-}
